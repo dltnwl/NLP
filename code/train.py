271e4a740429ec28617e2f0cdb71777e7f389afc
LEE <gagashi@naver.com>
2018-11-11 15:32:42 +0900

    Create train.py

---
+++ code/train.py
@@ -0,0 +1,148 @@
+import numpy as np
+wordsList = np.load('wordsList.npy')
+print('Loaded the word list!')
+wordsList = wordsList.tolist() #Originally loaded as numpy array
+wordsList = [word.decode('UTF-8') for word in wordsList] #Encode words as UTF-8
+wordVectors = np.load('wordVectors.npy')
+print ('Loaded the word vectors!')
+
+print(len(wordsList))
+print(wordVectors.shape)
+
+baseballIndex = wordsList.index('baseball')
+wordVectors[baseballIndex]
+
+import tensorflow as tf
+maxSeqLength = 10 #Maximum length of sentence
+numDimensions = 300 #Dimensions for each word vector
+
+
+
+with tf.Session() as sess:
+    print(tf.nn.embedding_lookup(wordVectors,firstSentence).eval().shape)
+    
+    from os import listdir
+from os.path import isfile, join
+
+
+
+
+
+
+maxSeqLength = 250
+
+        
+# Removes punctuation, parentheses, question marks, etc., and leaves only alphanumeric characters
+import re
+strip_special_chars = re.compile("[^A-Za-z0-9 ]+")
+
+
+ids = np.load('idsMatrix.npy')
+from random import randint
+
+def getTrainBatch():
+    labels = []
+    arr = np.zeros([batchSize, maxSeqLength])
+    for i in range(batchSize):
+        if (i % 2 == 0): 
+            num = randint(1,11499)
+            labels.append([1,0])
+        else:
+            num = randint(13499,24999)
+            labels.append([0,1])
+        arr[i] = ids[num-1:num]
+    return arr, labels
+
+def getTestBatch():
+    labels = []
+    arr = np.zeros([batchSize, maxSeqLength])
+    for i in range(batchSize):
+        num = randint(11499,13499)
+        if (num <= 12499):
+            labels.append([1,0])
+        else:
+            labels.append([0,1])
+        arr[i] = ids[num-1:num]
+    return arr, labels
+
+
+
+batchSize = 24
+lstmUnits = 64
+numClasses = 2
+iterations = 100000
+
+import tensorflow as tf
+tf.reset_default_graph()
+
+labels = tf.placeholder(tf.float32, [batchSize, numClasses])
+input_data = tf.placeholder(tf.int32, [batchSize, maxSeqLength])
+
+
+data = tf.Variable(tf.zeros([batchSize, maxSeqLength, numDimensions]),dtype=tf.float32)
+data = tf.nn.embedding_lookup(wordVectors,input_data)
+
+lstmCell = tf.contrib.rnn.BasicLSTMCell(lstmUnits)
+lstmCell = tf.contrib.rnn.DropoutWrapper(cell=lstmCell, output_keep_prob=0.75, variational_recurrent=True, dtype=tf.float32)
+value, _ = tf.nn.dynamic_rnn(lstmCell, data, dtype=tf.float32)
+
+
+weight = tf.Variable(tf.truncated_normal([lstmUnits, numClasses]))
+bias = tf.Variable(tf.constant(0.1, shape=[numClasses]))
+value = tf.transpose(value, [1, 0, 2])
+last = tf.gather(value, int(value.get_shape()[0]) - 1)
+prediction = (tf.matmul(last, weight) + bias)
+
+
+
+correctPred = tf.equal(tf.argmax(prediction,1), tf.argmax(labels,1))
+accuracy = tf.reduce_mean(tf.cast(correctPred, tf.float32))
+
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=labels))
+optimizer = tf.train.AdamOptimizer().minimize(loss)
+
+
+
+import datetime
+
+tf.summary.scalar('Loss', loss)
+tf.summary.scalar('Accuracy', accuracy)
+merged = tf.summary.merge_all()
+logdir = "tensorboard/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + "/"
+writer = tf.summary.FileWriter(logdir, sess.graph)
+
+
+
+sess = tf.InteractiveSession()
+saver = tf.train.Saver()
+sess.run(tf.global_variables_initializer())
+
+for i in range(iterations):
+   #Next Batch of reviews
+   nextBatch, nextBatchLabels = getTrainBatch();
+   sess.run(optimizer, {input_data: nextBatch, labels: nextBatchLabels})
+ 
+   #Write summary to Tensorboard
+   if (i % 50 == 0):
+       summary = sess.run(merged, {input_data: nextBatch, labels: nextBatchLabels})
+       writer.add_summary(summary, i)
+
+   #Save the network every 10,000 training iterations
+   if (i % 10000 == 0 and i != 0):
+       save_path = saver.save(sess, "models/pretrained_lstm_bayes.ckpt", global_step=i)
+       print("saved to %s" % save_path)
+writer.close()
+
+sess = tf.InteractiveSession()
+saver = tf.train.Saver()
+saver.restore(sess, tf.train.latest_checkpoint('models'))
+
+
+
+iterations = 10
+for i in range(iterations):
+    nextBatch, nextBatchLabels = getTestBatch();
+    print("Accuracy for this batch:", (sess.run(accuracy, {input_data: nextBatch, labels: nextBatchLabels})) * 100)
+    
+
+  
